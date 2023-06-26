83a21f2e1222904e403001bd8d99d1509b56f209
Nyetanyane-J <jnyetanyane2001@gmail.com>
2023-06-26 16:18:42 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -20,10 +20,11 @@ void puts2(char *str)
 	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
-		if (o % 2 == 0)
+		if (0 % 2 == 0)
 	{
-	_putchar(str[o]);
+		_putchar(str[0]);
 	}
 	}
 	_putchar('\n');
 }
+
